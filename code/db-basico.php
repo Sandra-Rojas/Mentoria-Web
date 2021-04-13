c9bf60d8bf573b32c05886d7cf16cb2bad8b2338
Sandra-Rojas <sandra.rojas@segic.usach.cl>
2021-04-13 15:20:57 -0400

    Insert a bd

--- code/db-basico.php
+++ code/db-basico.php
@@ -14,3 +14,27 @@ try {$dsn = "mysql:host=localhost;dbname=$dbname";
     echo $e->getMessage();
 
 }
+//preparar consulta
+$sql= "INSERT INTO users
+            (full_name, email, user_name, password)
+        values 
+            (:full_name, :email, :user_name, :password)";
+
+//statement
+$stmt = $db->prepare($sql);
+
+$full_name = 'Juan Perez';
+$email = 'juan.perez@segic.cl';
+$user_name='juan.perez';
+$password='juan123';
+
+$stmt -> bindParam (':full_name',$full_name);
+$stmt -> bindParam (':email',$email);
+$stmt -> bindParam (':user_name',$user_name);
+$stmt -> bindParam (':password',$password);
+
+$stmt -> execute();
+
+
+
+

99a8f065c4b88a9389783da8112696cc0fe6ced9
Sandra-Rojas <sandra.rojas@segic.usach.cl>
2021-04-16 12:49:52 -0400

    Conexion a bd

--- code/db.php
+++ code/db.php
@@ -1,19 +1,21 @@
 <?php
 
-function connectDB(){
-$dbname = "registro"; //modicar por valor no valido y comprobar try
-$dbuser = "registro_user";
-$dbpassword = "registro_user1";
+function connectDB()
+{
+    $dbname = "registro"; //modicar por valor no valido y comprobar try
+    $dbuser = "registro_user";
+    $dbpassword = "registro_user1";
 
-try {
+    try {
     
-    $dsn = "mysql:host=localhost;dbname=$dbname";
-    //objeto de conexion a la bd
-    $db = new PDO($dsn, $dbuser, $dbpassword);
-    
-}catch(PDOException $e){
-    echo $e->getMessage();
+        $dsn = "mysql:host=localhost;dbname=$dbname";
+        //objeto de conexion a la bd
+        $db = new PDO($dsn, $dbuser, $dbpassword);
+        return $db;
+
+    }   catch(PDOException $e){
+        echo $e->getMessage();
 }
 
-}
+
 

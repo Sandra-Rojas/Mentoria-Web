d33b0429d2446ec1dfa75572428d1d0125d4fecd
Sandra-Rojas <sandra.rojas@segic.usach.cl>
2021-04-23 00:19:27 -0400

    Comprobacion del resultado de select

--- code/index.php
+++ code/index.php
@@ -13,6 +13,16 @@ if (isset($_POST['sing-in-button'])) {
 	$username = $_POST['username'];
 	$password = $_POST['pass'];
 
+	$sql = "Select * FROM users WHERE username = '$username'";
+	$result = $db->query($sql);
+
+	if ($result){
+		//$row = $result->fetch_row();
+		echo "El result existe";
+	}else{
+		echo "El result no existe";
+	}
+
 	echo "$username, $password ";
 
 }

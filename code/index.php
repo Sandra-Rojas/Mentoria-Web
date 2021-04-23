3b48785de8733b034909918c7a6d6ab0e70f9b34
Sandra-Rojas <sandra.rojas@segic.usach.cl>
2021-04-23 12:19:10 -0400

    Imprime contenido de select con print_r

--- code/index.php
+++ code/index.php
@@ -44,7 +44,7 @@ if (isset($_POST['sing-in-button'])) {
 	//modifica sentencia result, se debe validar tb que query devuelve datos
 	if ($result) {
 		if ($row = $result->fetch_row()){
-			echo "\nDatos de query: " 
+			echo "\nDatos de query: ";
 			print_r($row);
 			echo "\nEl result existe\n";
 			}

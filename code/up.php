1f4e6710bad71af2e85f6b4263fd40f8c8923276
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-04-29 00:37:56 -0400

    Implemeta subir archivo

--- code/up.php
+++ code/up.php
@@ -7,7 +7,7 @@ $target_file = $target_dir . basename($_FILES["imagen"]["name"]);
 
 //mueve el arhivo desde la direccion temporal del servidor a carpeta uploads
 //tmp_name tiene la direccion temporal de archivo servidor
-print_r($_FILES["imagen"]["tmp_name"]);
+print_r($_FILES["imagen"]);
 if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
     echo "The file  has been uploaded.";
   } else {

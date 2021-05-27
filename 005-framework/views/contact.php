9a49c718f0aa45519899ba1ebf354258108bbffb
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-05-27 12:13:26 -0400

    modifica contact

--- 005-framework/views/contact.php
+++ 005-framework/views/contact.php
@@ -1,20 +1,19 @@
 <h1>Contact Us</h1>
-<format method="POST">
-<form>
+<form method="POST">
   <div class="mb-3">
     <label class="form-label">Subject</label>
     <input type="text" name = "subject" class="form-control" >
-</div>
-<div class="mb-3">
+  </div>
+  <div class="mb-3">
     <label class="form-label">Email</label>
     <input type="text" name = "email" class="form-control" >
-</div>
-<div class="mb-3">
+  </div>
+  <div class="mb-3">
     <label class="form-label">Body</label>
     <textarea name="body" class="form-control" id="" cols="30" rows="10"></textarea>
-</div>
-
-<button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
  

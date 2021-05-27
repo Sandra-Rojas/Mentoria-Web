5c88436737afa61fd03236dc9cd06613683b3970
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-05-27 11:53:57 -0400

    modifica contac elimina clase Segic

--- 005-framework/views/contact.php
+++ 005-framework/views/contact.php
@@ -13,7 +13,7 @@
     <label class="form-label">Body</label>
     <textarea name="body" class="form-control" id="" cols="30" rows="10"></textarea>
 </div>
-<div class="segic"></div>
+
 <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 

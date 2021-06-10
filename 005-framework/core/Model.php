11e565dedb9442ff98dd9664c1ba393ad0b9dfbe
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-06-10 12:15:30 -0400

    Agrega invalido feedback

--- 005-framework/core/Model.php
+++ 005-framework/core/Model.php
@@ -72,7 +72,7 @@ abstract class Model
     {
         $message = $this->errorMessages()[$rule] ?? '';
         
-        foreach($params as $key => $param){
+        foreach($params as $key => $params){
             $message = str_replace("{{$key}}", $params, $message);
         }
         

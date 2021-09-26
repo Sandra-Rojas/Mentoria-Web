23094a94baf45dcb3873d20d121f16674fabdae2
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-09-26 17:31:30 -0300

    Modifica seeder

--- 009-laravel/blog/database/seeders/DatabaseSeeder.php
+++ 009-laravel/blog/database/seeders/DatabaseSeeder.php
@@ -19,33 +19,38 @@ class DatabaseSeeder extends Seeder
 
         User::truncate();
         Category::truncate();
+        Post::truncate();
+
+        Post::factory()->create();
+        //se comenta para agregar truncate de Post llamar a post factory  para crear una cantidad de post
          //\App\Models\User::factory(10)->create();
          //\App\Models\User::factory()->create();
 
-         $user = User::factory()->create();
+         // $user = User::factory()->create();
 
-         $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-         ]);
+         // $personal = Category::create([
+         //    'name' => 'Personal',
+         //    'slug' => 'personal',
+         // ]);
 
-         $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'worh',
-         ]);
+         // $work = Category::create([
+         //    'name' => 'Work',
+         //    'slug' => 'worh',
+         // ]);
          
-         $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies',
-         ]);
+         // $hobbies = Category::create([
+         //    'name' => 'Hobbies',
+         //    'slug' => 'hobbies',
+         // ]);
 
-         Post::create([
-             'category_id' => $work->id,
-             'user_id' => $user->id,
-             'slug' => 'my-firt-post',
-             'title' => 'My First Post',
-             'resumen' => 'The are many',
-             'body' => 'Teha are many viartions',
-         ]);
-        }
+         // Post::create([
+         //     'category_id' => $work->id,
+         //     'user_id' => $user->id,
+         //     'slug' => 'my-firt-post',
+         //     'title' => 'My First Post',
+         //     'resumen' => 'The are many',
+         //     'body' => 'Teha are many viartions',
+         // ]);
+        
+      }
 }

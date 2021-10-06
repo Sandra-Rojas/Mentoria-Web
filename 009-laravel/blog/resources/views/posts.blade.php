1d907425dc6a5b29de64a251d6d0ba6e4077f896
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-06 16:57:15 -0300

    components

--- 009-laravel/blog/resources/views/posts.blade.php
+++ 009-laravel/blog/resources/views/posts.blade.php
@@ -1,5 +1,11 @@
-<x-layout>
+<!-- primera forma -->
+<x-layout content = "Hola Segic!" >
+</x-layout>
+
+
+<!-- segunda forma -->
+<!-- <x-layout >
     <x-slot name = "content">
         Hola SEGIC!
     </x-slot>
-</x-layout>
+</x-layout> -->

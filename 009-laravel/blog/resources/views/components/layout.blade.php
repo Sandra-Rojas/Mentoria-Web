6fc66d93798a53e1d4e55f9cb61f4c90412ee207
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-06 17:23:50 -0300

    components utilizando variable slots

--- 009-laravel/blog/resources/views/components/layout.blade.php
+++ 009-laravel/blog/resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    {{ $content }}
+    <!-- {{ $content }} -->
+    {{ $slot }}
 </body>
 </html>
--- 009-laravel/blog/resources/views/posts.blade.php
+++ 009-laravel/blog/resources/views/posts.blade.php
@@ -1,11 +1,17 @@
-<!-- primera forma -->
-<x-layout content = "Hola Segic!" >
-</x-layout>
+<!-- primera forma, utilizando content en layout.blade -->
+<!-- <x-layout content = "Hola Segic!" >
+</x-layout> -->
 
 
-<!-- segunda forma -->
+<!-- segunda forma, utilizando content en layout.blade --> -->
 <!-- <x-layout >
     <x-slot name = "content">
         Hola SEGIC!
     </x-slot>
 </x-layout> -->
+
+<!-- tercera forma, utilizando variable sltot en layout.blade --> -->
+<x-layout >
+        Hola SEGIC! Estoy utilizando variable slot
+</x-layout>
+

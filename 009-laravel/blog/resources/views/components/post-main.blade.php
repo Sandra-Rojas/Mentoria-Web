34b5edc3e1fe87b296b09912ff6ff300be56fe9b
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-06 20:34:47 -0300

    Trabaja en component main

--- 009-laravel/blog/resources/views/components/post-main.blade.php
+++ 009-laravel/blog/resources/views/components/post-main.blade.php
@@ -1,4 +1,4 @@
-@props[['post']]
+@props(['post'])
 <article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5 lg:flex">
         <div class="flex-1 lg:mr-8">
@@ -28,10 +28,6 @@
             <div class="text-sm mt-2">
                 <p>
                     {{ $post->resumen }}
-                </p>
-
-                <p class="mt-4">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 </p>
             </div>
 

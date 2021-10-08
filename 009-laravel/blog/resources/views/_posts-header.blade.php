c54b506e187d3f6cc7e8645b56a91b3b82b5efcc
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-08 18:29:03 -0300

    Comenzamos a trabajar con el component de categorias

--- 009-laravel/blog/resources/views/_posts-header.blade.php
+++ 009-laravel/blog/resources/views/_posts-header.blade.php
@@ -27,7 +27,7 @@
 
             <!-- Segunda forma utilizando biblicoteca de Aplphine, componete de Aplhine -->
             <div x-data= "{ show:false }">
-                <button @click="show= true">Category</button>
+                <button @click="show= !show">Category</button>
 
                 <a href="#" x-show="show">One</a> 
                 <a href="#" x-show="show">Two</a>

6bbe9283dd4a3a49cbeeb88103cedca08a80c699
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-08 17:34:56 -0300

    Inclute Alphine al proyecto

--- 009-laravel/blog/resources/views/_posts-header.blade.php
+++ 009-laravel/blog/resources/views/_posts-header.blade.php
@@ -15,16 +15,6 @@
         <!--  Category -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <!-- Primera forma utilizando foreach -->
-            <!-- <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>
-                    Category
-                </option>
-                    @foreach ($categories as $category)
-                        <option value = "{{ $category->slug}}">{{ $category->name}} </option>
-                    @endforeach
-
-            </select> -->
-
             <!-- Segunda forma utilizando biblicoteca de Aplphine, componete de Aplhine -->
             <div x-data>
                 <button>Category</button>

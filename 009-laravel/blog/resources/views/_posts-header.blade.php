34f026075cd90e0aec4e0bcf323f4a15776d5a14
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-19 15:41:55 -0300

    Modifica css

--- 009-laravel/blog/resources/views/_posts-header.blade.php
+++ 009-laravel/blog/resources/views/_posts-header.blade.php
@@ -19,6 +19,7 @@
             <div x-data= "{ show:false }">
                 <button @click="show= !show" class= "py-2 pl-3 pr-9 text-sm font-semibold inline-flex">
                     Category
+
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                             height="22" viewBox="0 0 22 22">
                         <g fill="none" fill-rule="evenodd">
@@ -31,7 +32,7 @@
 
                 </button>
 
-                <div x-show class = "py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 text-left px-3">
+                <div x-show class = "py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 text-left px-3 text-sm style="display: none;">
                     <a href="#" class = "block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">One</a> 
                     <a href="#" class = "block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">Two</a>
                     <a href="#" class = "block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">Three</a>

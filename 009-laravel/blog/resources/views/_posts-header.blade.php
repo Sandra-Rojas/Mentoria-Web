ad10c842147ddd7079f28d908ca608fff3f98e23
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-22 15:05:24 -0300

    fix select category

--- 009-laravel/blog/resources/views/_posts-header.blade.php
+++ 009-laravel/blog/resources/views/_posts-header.blade.php
@@ -66,7 +66,7 @@
         </div>
 
         <!-- Search -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
+        <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
                 <input type="text" name="search" placeholder="Find something"
                         class="bg-transparent placeholder-black font-semibold text-sm">

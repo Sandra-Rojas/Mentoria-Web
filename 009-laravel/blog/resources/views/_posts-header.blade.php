508480a4517c04914cd1e2c889715502323e1d06
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-10-25 13:00:16 -0300

    sintaxis posts header blade

--- 009-laravel/blog/resources/views/_posts-header.blade.php
+++ 009-laravel/blog/resources/views/_posts-header.blade.php
@@ -6,24 +6,31 @@
      <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
-            <x-dropdown>
-                <x-slot name= "trigger">
-                    <button class= "py-2 pl-3 pr-9 text-sm font-semibold lg:w-32 w-full flex lg:inline-flex">
-                        {{ isset($currentCategory) ? ucwords(($currentCategory->name)) : 'Categories' }}
-
-                        <x-icon name="down-arrow" class = "absolute pointer-events-none" style="right: 12px;" />
+        <x-dropdown>
+                <x-slot name="trigger">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold  lg:w-32 w-full flex lg:inline-flex">
+                        {{ isset( $currentCategory ) ? ucwords($currentCategory->name) : "Categories"}}
+                        
+                        <x-icon name="down-arrow" class="absolute pointer-events-none" style="right:12px " />
+                        
                     </button>
+
                 </x-slot>
 
-                <x-dropdown-item href = "/" :actived="request()->routeIs('home')">ALL</x-dropdown-item>
+                
 
-                @foreach( $categories as $category)
-                <x-dropdown-item 
+                <x-dropdown-item href="/" :actived="request()->routeIs('home')">
+                     All
+                </x-dropdown-item>
+
+                @foreach ($categories as $category)
+                    <x-dropdown-item 
                         href="/category/{{ $category->slug }}" 
                         :actived="isset($currentCategory) && $currentCategory->is($category)"
                         > {{ ucwords($category->name) }}
-                </x-dropdown-item>
+                    </x-dropdown-item>
                 @endforeach
+
             </x-dropdown>
         </div>
 

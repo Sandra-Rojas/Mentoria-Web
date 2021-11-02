b57f7246de5d72a27f9208e4da6d9d2d90870376
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-11-02 12:52:43 -0300

    Se guarda la busqueda del filtro en la vista

--- 009-laravel/blog/resources/views/_posts-header.blade.php
+++ 009-laravel/blog/resources/views/_posts-header.blade.php
@@ -40,7 +40,8 @@
                 <input type="text" 
                         name="search" 
                         placeholder="Find something"
-                        class="bg-transparent placeholder-black font-semibold text-sm">
+                        class="bg-transparent placeholder-black font-semibold text-sm"
+                        value ="{{ request('search') }}">
             </form>
         </div>
     </div>

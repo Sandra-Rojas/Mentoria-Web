b9551eb6b4f7a5cf817cd9b512a74773d8919367
sandra.rojas <sandra.rojas@segic.usach.cl>
2021-11-09 16:16:43 -0300

    Agrega currentCategrory al controlador

--- 009-laravel/blog/app/Http/Controllers/PostController.php
+++ 009-laravel/blog/app/Http/Controllers/PostController.php
@@ -11,24 +11,15 @@ class PostController extends Controller
     public function index()
     {
         return view('posts', [
-            //'posts' => Post::latest('published_at')->with(['category', 'author'])->filter()->get(),
-            'posts' => Post::latest('published_at')
-                        ->filter(request(['search', 'category']))
-                        ->get(),
+        //'posts' => Post::all()
+        //'posts' => $posts
+            'posts' => Post::latest('published_at') //Ordenamiento
+                ->filter(request(['search', 'category']))
+                ->get(), //get => ejecutar
             'categories' => Category::all(),
-            'currentCategory' => Category::where('slug', request('category'->first))
-            //'test'  => 'bla bla',
-            //'posts' => collect([]), //simular que no hay data    
-    
-            //'posts' => Post::with('category')->get()
-            //agrega orden, el ultimo en publicar encabeza listado de post
-            //'posts' => Post::latest('published_at')
-            //para que la consulta a la bd realice una precarga, y no consulte uno a uno los uusarios, se agrega arreglo con user
-            //with es utilizado sólo cuando se llama estaticamente al modelo
-            //    ->with('category')
-            //->with(['category', 'user'])
+            'currentCategory' => 
+                request('category') !== null ? Category::where('slug', request('category'))->first() : null,
         ]);
-
     }
 
     Public function show(Post $post) {    
